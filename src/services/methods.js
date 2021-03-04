6b15cb5f26453ce80b5700333f625b715231e896
Amin Abbasi <amin4193@gmail.com>
2021-03-04 19:12:09 +0330

    add replacement functions for lodash in global methods

--- src/services/methods.js
+++ src/services/methods.js
@@ -32,6 +32,45 @@ module.exports = (config) => {
     catch (e) { return false }
   }
 
+  /**
+   * Set Unique Array Function
+   * @param array array of string to be checked
+   */
+  function setUniqueArray(array) {
+    return array.filter((value, index, self) => self.indexOf(value) === index)
+  }
+
+  /**
+   * Simple object check.
+   * @param item
+   * @returns {boolean}
+   */
+  function isObject(item) {
+    return (item && typeof item === 'object' && !Array.isArray(item))
+  }
+
+  /**
+   * Deep merge two objects.
+   * @param target
+   * @param ...sources
+   */
+  function mergeDeep(target, ...sources) {
+    if (!sources.length) return target
+    const source = sources.shift()
+
+    if (isObject(target) && isObject(source)) {
+      for (const key in source) {
+        if (isObject(source[key])) {
+          if (!target[key]) Object.assign(target, { [key]: {} })
+          mergeDeep(target[key], source[key])
+        } else {
+          Object.assign(target, { [key]: source[key] })
+        }
+      }
+    }
+    return mergeDeep(target, ...sources)
+  }
+
   // JWT Token Functions
   const jwt = {
     // Creates JWT Token
